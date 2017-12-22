3e372acf2a8ff0ee1dc083d151a257dec728afb5
Praktykant ideo 1 <ideo1@ideo.local>
2017-12-22 10:39:57 +0100

    Modified searchform and fixed some issues. Fatal problem with content/search implementation. 3 hours without productivity. Great. Good job

--- searchform.php
+++ searchform.php
@@ -1,13 +1,11 @@
-<form action="http://wp-szkolenie-3.t.ideo/wordpress/" role="search" method="get" class="navbar-form navbar-right navbar-form-search formSearch hidden-md hidden-sm hidden-xs">
+<form action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search" method="get" class="navbar-form navbar-right navbar-form-search formSearch hidden-md hidden-sm hidden-xs">
     <div class="search-form-container hdn" id="search-input-container">
         <div class="search-input-group">
             <button type="button" class="btn btn-default" id="hide-search-input-container">
                 <i class="fa fa-ellipsis-h" aria-hidden="true"></i>
             </button>
-            <input type="hidden" value="1" name="sentence" />
-            <input type="hidden" value="product" name="post_type" />
             <div class="form-group">
-                <input type="text" name="search_input" class="form-control" id="search_input" placeholder="Search for..." autofocus>
+                <input type="text" value="<?php echo get_search_query(); ?>" name="s" class="form-control" id="search_input" placeholder="Search for..." autofocus>
             </div>
 
         </div>
