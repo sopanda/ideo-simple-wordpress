d6f22c7316887b913e97b84115335fc4fbc720ad
Praktykant ideo 1 <ideo1@ideo.local>
2017-12-28 14:40:32 +0100

    Beta version of Branze section - done. Responsive. But i'm not sure for it

--- index.php
+++ index.php
@@ -8,6 +8,9 @@
 
 <!-- Content Section -->
 <main>
+    <?php the_field('branze'); ?>
+    <?php the_field('produkty'); ?>
+    <?php the_field('uslugi'); ?>
     <?php dynamic_sidebar( 'custom-side-bar' ); ?>
 </main>
 <!-- Content Ends -->
